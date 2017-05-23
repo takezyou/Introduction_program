77dabfd08922504e28cb665ef34af57ec7b2fc9a
takezyou <kaitokun07@icloud.com>
2017-05-24 01:33:21 +0900

    modify 2-4(3).c

--- result/2-4(3).c
+++ result/2-4(3).c
@@ -3,12 +3,10 @@
 
 int main(void){
     int i, n;
-    double pi, a, b, c, d, fact1, fact2;
+    double pi, a, b, fact1, fact2;
     pi = 0;
     a = 0;
     b = 0;
-    c = 0;
-    d = 1;
     fact1 = 1.0;
     fact2 = 1.0;
 
@@ -16,16 +14,15 @@ int main(void){
 	scanf("%d",&n);
 
     for(i = 0; i < n; i++){
-        a += (fact1 / fact2) * pow((1.0 / 10.0), i);
-        b += (fact1 / fact2) * pow((2.0 / 10.0), i);
-        pi =  ((3.0 / 10.0) * a) + ((4.0 / 10.0) * b);
+        a += (fact1 / fact2) * pow(0.1, i);
+        b += (fact1 / fact2) * pow(0.2, i);
+        pi =  (0.3 * a) + (0.4 * b);
 
         printf("i = %d, π / 4 = %lf\n", i, pi);
 
-        fact1 = fact1 * (2.0 * n + c);
-        c = c - 2;
+        fact1 *= (2.0 * (double)i + 2); 
 
-        fact2 = fact2 * (2.0 * n  + d);
-        d = d - 2;
+        fact2 *= (2.0 * (double)i  + 3);
+
     }
 }
